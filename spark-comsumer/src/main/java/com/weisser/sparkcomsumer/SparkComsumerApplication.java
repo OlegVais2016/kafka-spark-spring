1240e2b7664d8c9da768d4a5159581b40e1e914c
Oleg Vaiser <aly9681082@gmail.com>
2021-04-20 20:24:55 +0300

    first commit

---
+++ spark-comsumer/src/main/java/com/weisser/sparkcomsumer/SparkComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.weisser.sparkcomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkComsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkComsumerApplication.class, args);
+	}
+
+}
---
+++ spark-comsumer/src/test/java/com/weisser/sparkcomsumer/SparkComsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.sparkcomsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkComsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
